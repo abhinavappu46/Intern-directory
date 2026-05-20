22a93339f0cc59d385ee0392f983f182db3bb30f
abhinavappu46 <abhinav@gmail.com>
2026-05-20 23:21:34 +0530

    Formatted

--- src/InternForm.jsx
+++ src/InternForm.jsx
@@ -5,37 +5,37 @@ import StudentDetail from './StudentDetail';
 function InternFrom() {
   const [Id, setId] = useState(0);
   const [SendData, setSendData] = useState([])
- const[BUttonDisable,setButtonDisable]=useState(false)
+  const [BUttonDisable, setButtonDisable] = useState(false)
   const HandleLogin = (e) => {
     const alreadyExists = SendData.some(
-    (student) => student.Email === FormData.Email
-  );
+      (student) => student.Email === FormData.Email
+    );
 
-  if (alreadyExists) {
+    if (alreadyExists) {
 
-    setButtonDisable(true);
+      setButtonDisable(true);
 
-    alert("Student already exists!");
- 
-  }
-    const NewData = ({ ...FormData, id: Id + 1 ,status:false});
+      alert("Student already exists!");
+
+    }
+    const NewData = ({ ...FormData, id: Id + 1, status: false });
 
     e.preventDefault();
     setSendData([...SendData, NewData]);
     setId(Id + 1);
   }
-  
-  
+
+
   const DeleteIntern = (id) => {
     const UpdatedData = SendData.filter((student) => student.id !== id);
     setSendData(UpdatedData);
   }
 
-    const HandleToggle =(id)=>{
-      const NewData= SendData.map((student)=>student.id ===id ? {...student, status: !student.status}:student);
-    
+  const HandleToggle = (id) => {
+    const NewData = SendData.map((student) => student.id === id ? { ...student, status: !student.status } : student);
+
     setSendData(NewData);
-    }
+  }
 
   const [FormData, setFormData] = useState({
     id: "",
@@ -43,45 +43,45 @@ function InternFrom() {
     Email: "",
     phone: "",
     Qualification: "",
-    role:"",
+    role: "",
   })
 
 
   return (
     <div id='TopContainer'>
       <div id="Navbar">
-       <h1>INTERN ADDING INTERN</h1>
-       <nav className="navbar navbar-expand-lg ">
-  <div class="container-fluid">
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <h1>INTERN ADDING INTERN</h1>
+        <nav className="navbar navbar-expand-lg ">
+          <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
       <div className='FromContainer'>
         <form action="" onSubmit={HandleLogin}>
@@ -95,9 +95,9 @@ function InternFrom() {
           <label >Qualification:&nbsp;</label>
           <input name="Qualification" type="text" placeholder='enter your qualification' className='InputConatiner' onChange={(text) => { setFormData({ ...FormData, [text.target.name]: text.target.value }) }} />
           <label>Role:</label>
-<input name="role" type="text" placeholder='Enter role' className='InputConatiner' onChange={(text) => { setFormData({ ...FormData, [text.target.name]: text.target.value }) }} />
+          <input name="role" type="text" placeholder='Enter role' className='InputConatiner' onChange={(text) => { setFormData({ ...FormData, [text.target.name]: text.target.value }) }} />
           <button type='submit' id='Btn1' onClick={HandleLogin} disabled={BUttonDisable}>Add Member</button>
-          
+
         </form>
 
       </div>

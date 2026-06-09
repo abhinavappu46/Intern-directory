0d979c1280e1268cda09e8d2c0eac47fe1861758
abhinavappu46 <abhinav@gmail.com>
2026-06-09 16:41:46 +0530

    url modified

--- src/axios api/Api.js
+++ src/axios api/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://intern-directory-backend-1.onrender.com"
 });
 export default api;
